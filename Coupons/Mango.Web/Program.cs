ff7e16eac4b3822fb9b47b0c6efe74c9aee684b2
ochoaluiscepi <37229115+ochoaluiscepi@users.noreply.github.com>
2025-03-23 15:19:00 -0600

    Update Program.cs

--- Coupons/Mango.Web/Program.cs
+++ Coupons/Mango.Web/Program.cs
@@ -18,7 +18,6 @@ builder.Services.AddScoped<ICouponService, CouponService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
