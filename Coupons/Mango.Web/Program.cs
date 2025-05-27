601453e53191f02d5a1a47e8dbba9868a336d5e2
ochoaluiscepi <37229115+ochoaluiscepi@users.noreply.github.com>
2025-05-26 19:08:09 -0600

    Update Program.cs

--- Coupons/Mango.Web/Program.cs
+++ Coupons/Mango.Web/Program.cs
@@ -21,7 +21,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    
     app.UseHsts();
 }
 
